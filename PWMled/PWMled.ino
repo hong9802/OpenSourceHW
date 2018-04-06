e57c6e9f49556a3bc99fcb56fb52d2438d5c84b7
hong9802 <hlee993@gmail.com>
2018-04-06 23:14:54 +0900

    first commit

---
+++ PWMled/PWMled.ino
@@ -0,0 +1,32 @@
+int led[2] = {11, 10};
+int bright = 0;
+int bright2 = 255;
+int brightstep = 5;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(led[0], OUTPUT);
+  pinMode(led[1], OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    for(int j = 0; j < 255; j+=5)
+    {
+      analogWrite(led[0], bright);
+      analogWrite(led[1], bright2);
+      bright += brightstep;
+      bright2 -= brightstep;
+      delay(50);
+    }
+    for(int j = 250; j > -1; j-=5)
+    {
+      analogWrite(led[0], bright);
+      analogWrite(led[1], bright2);
+      bright -= brightstep;
+      bright2 += brightstep;
+      delay(50);
+    }
+    analogWrite(led[0], 0);
+    analogWrite(led[1], 255);
+}
---
+++ led3traffic/led3traffic.ino
@@ -0,0 +1,33 @@
+int green = 12;
+int yellow = 8;
+int red = 7;
+int i = 0;
+int ledtime[4] = {1000, 500, 250, 125};
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(green, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(red, OUTPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(i = 0; i < 4; i++)
+  {
+    if(i == 4) 
+    {
+      i = 0;
+      break;
+    }
+    digitalWrite(green, HIGH);
+    delay(ledtime[i]);
+    digitalWrite(green, LOW);
+    digitalWrite(yellow, HIGH);
+    delay(ledtime[i]);
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, HIGH);
+    delay(ledtime[i]);
+    digitalWrite(red, LOW);
+    Serial.println(i);
+  }
+}
